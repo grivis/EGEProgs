1ba717d51c6f2b099f9ed7b98107630f5031efb6
grigory <sss>
2016-10-16 08:52:41 +0300

    Правка

--- PatternWeb.py
+++ PatternWeb.py
@@ -1,9 +1,6 @@
 import urllib.request  # импортируем модуль
 import re
-# req = urllib.request.Request('https://habrahabr.ru/')
-# with urllib.request.urlopen(req) as response:
-#    html = response.read().decode('utf-8')
-# print(html[:250])
+
 
 url = 'https://habrahabr.ru/'  # адрес страницы, которую мы хотим скачать
 user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'  # хотим притворяться браузером
@@ -20,7 +17,7 @@ titles = regPostTitle.findall(html)
 
 
 print(len(titles))
-#print(titles[:3])
+
 
 new_titles = []
 regTag = re.compile('<.*?>', flags=re.U | re.DOTALL)
@@ -29,9 +26,6 @@ for t in titles:
     clean_t = regSpace.sub("", t)
     clean_t = regTag.sub("", clean_t)
     new_titles.append(clean_t)
-# for t in new_titles:
-#     print(t)
-
 
 for t in new_titles:
     print(t.replace("&nbsp;&rarr;", " -> "))
