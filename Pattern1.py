24a34218ef17a354653132e9719d11b11099bad9
grigory <sss>
2016-10-16 08:50:07 +0300

    Правка

--- Pattern1.py
+++ Pattern1.py
@@ -1,7 +1,17 @@
 import re
-f = open('wordread.txt')
-read = f.read()
+
+read = 'А я сижу на берегу. Сидя у окна, считаю ворон. Сидит девица в терему. Сидючи дома, мир не увидишь.'
 result = re.findall(r'[Сс]и[дж][а-я]*', read)
-resset = set(result)
-for word in resset:
+for word in result:
     print(word)
+
+
+print()
+
+read1 = 'Я пришел, а ты ушел. Петя зашел в подъезд. Сначала вошел, потом вышел. Его нет, он отошел.'
+result1 = re.findall(r'[при|во|за|ото]+шел', read1)
+for word in result1:
+    print(word)
+
+print()
+
