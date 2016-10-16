9c9f02077f21e46d5f806b7e65848179abb78aa3
grigory <sss>
2016-10-16 08:53:55 +0300

    Правка

--- Pattern1.py
+++ Pattern1.py
@@ -1,7 +1,7 @@
 import re
 
 read = 'А я сижу на берегу. Сидя у окна, считаю ворон. Сидит девица в терему. Сидючи дома, мир не увидишь.'
-result = re.findall(r'[Сс]и[дж][а-я]*', read)
+result = re.findall('[Сс]и[дж][а-я]*', read)
 for word in result:
     print(word)
 
@@ -9,7 +9,7 @@ for word in result:
 print()
 
 read1 = 'Я пришел, а ты ушел. Петя зашел в подъезд. Сначала вошел, потом вышел. Его нет, он отошел.'
-result1 = re.findall(r'[при|во|за|ото]+шел', read1)
+result1 = re.findall('[при|во|за|ото]+шел', read1)
 for word in result1:
     print(word)
 
